7b1f470034b1fe27f3174313ac52e362179c0e2d
Andy James <andrewhayesjames@gmail.com>
2017-10-17 00:22:38 -0400

    Add Helvetica font for android

--- App.js
+++ App.js
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import LifeScreen from "./src/counter/counter";
 
+import { Font } from "expo";
+
 export default class App extends Component {
+    state = {
+        fontLoaded: false,
+    };
+    async componentDidMount() {
+        await Font.loadAsync({
+            Helvetica: require("./src/resources/fonts/HELR65W.ttf"),
+            "Helvetica-Bold": require("./src/resources/fonts/HELR65W.ttf"),
+        });
+
+        this.setState({ fontLoaded: true });
+    }
     render() {
         return (
             <View style={{ flex: 1 }}>
                 <StatusBar hidden={true} />
-                <LifeScreen startingLife={20} />
+                {this.state.fontLoaded ? <LifeScreen startingLife={20} /> : null}
             </View>
         );
     }
