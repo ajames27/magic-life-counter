890635238825a798ae8dd917e8c7ab71857b4cf5
Andy James <andrewhayesjames@gmail.com>
2018-01-01 01:37:02 -0500

    Start adding settings

---
+++ src/drawer/listItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { View, Text, Dimensions } from "react-native";
+const { height: screenHeight, width: screenWidth } = Dimensions.get("window");
+
+const ListItem = text => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                height: 40,
+                width: screenWidth,
+                borderColor: "#000",
+                borderBottomWidth: 1,
+                justifyContent: "center",
+            }}
+        >
+            <Text style={{ marginLeft: 10 }}>{text.key}</Text>
+        </View>
+    );
+};
+
+export default ListItem;
---
+++ src/drawer/settings.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { FlatList, Text } from "react-native";
+import ListItem from "./listItem";
+
+class Settings extends Component {
+    state = {};
+    render() {
+        return <FlatList data={[{ key: "a" }, { key: "b" }]} renderItem={item => <ListItem text={item} />} />;
+    }
+}
+
+export default Settings;
